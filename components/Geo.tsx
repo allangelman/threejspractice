09889fdf517a3d329f0a27209ed7d2e9ef697de9
Allan Gelman <allangelman4@gmail.com>
2023-01-08 01:13:29 -0500

    orbit controls and adding light to camera

--- components/Geo.tsx
+++ components/Geo.tsx
@@ -1,13 +1,17 @@
-import { ReactElement, useState } from "react";
+import { ReactElement, useRef, useState } from "react";
 import { useFrame, useThree } from "@react-three/fiber";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { PointLight, Scene } from "three";
 
 export const Geometry = (): ReactElement => {
-  const { camera, gl } = useThree();
+  const { scene, camera, gl } = useThree();
   const controls = new OrbitControls(camera, gl.domElement);
+  const pointLightRef = useRef<PointLight>(null);
   controls.enableDamping = true;
   controls.rotateSpeed = 0.5;
   controls.zoomSpeed = 0.1;
+  scene.add(camera);
+  if (pointLightRef.current) camera.add(pointLightRef.current);
   const [boxPosition, setBoxPosition] = useState<{
     x: number;
     y: number;
@@ -29,7 +33,7 @@ export const Geometry = (): ReactElement => {
 
   return (
     <>
-      <pointLight position={[-5, -4, 10]} />
+      <pointLight ref={pointLightRef} position={[-5, -4, 10]} />
       <mesh
         position={[boxPosition.x, boxPosition.y, boxPosition.z]}
         // rotation={[boxPosition.x, boxPosition.y, boxPosition.z]}
