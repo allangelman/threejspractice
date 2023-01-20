b2981d9be1382af2ba6420f45efc1ffdbcd72f55
Allan Gelman <allangelman4@gmail.com>
2023-01-20 15:30:22 -0500

    descriptions for markers and trying to get new markers

--- components/Geo.tsx
+++ components/Geo.tsx
@@ -1,7 +1,15 @@
-import { ReactElement, RefObject, Suspense, useRef, useState } from "react";
+import {
+  Dispatch,
+  ReactElement,
+  RefObject,
+  SetStateAction,
+  Suspense,
+  useRef,
+  useState,
+} from "react";
 import { useFrame, useThree } from "@react-three/fiber";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import { PointLight, Vector3, Raycaster } from "three";
+import { PointLight, Vector3, Vector2, Raycaster } from "three";
 import { LoadingSkeleton, Model } from "./Model";
 
 interface point {
@@ -10,22 +18,74 @@ interface point {
 }
 interface GeometryProps {
   points: point[];
+  setPoints: Dispatch<SetStateAction<Vector3[]>>;
+  currentPoints: Vector3[];
 }
+const pointer = new Vector2();
 const raycaster = new Raycaster();
-export const Geometry = ({ points }: GeometryProps): ReactElement => {
+
+export const Geometry = ({
+  points,
+  setPoints,
+  currentPoints,
+}: GeometryProps): ReactElement => {
   const { scene, camera, gl } = useThree();
   const controls = new OrbitControls(camera, gl.domElement);
   const pointLightRef = useRef<PointLight>(null);
   controls.enableDamping = true;
-  controls.rotateSpeed = 0.5;
-  controls.zoomSpeed = 0.1;
+  controls.rotateSpeed = 0.005;
+  controls.zoomSpeed = 0.005;
   scene.add(camera);
   if (pointLightRef.current) camera.add(pointLightRef.current);
 
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
 
+  const [currentPointerPosition, setCurrentPointerPosition] =
+    useState<Vector3>();
+
+  function onPointerMove(event: { clientX: number; clientY: number }) {
+    // calculate pointer position in normalized device coordinates
+    // (-1 to +1) for both components
+
+    pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+    pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  }
+
+  function onPointerDown(event: { clientX: number; clientY: number }) {
+    if (currentPointerPosition) {
+      // console.log(currentPointerPosition);
+      // console.log(
+      //   new Vector3(
+      //     currentPointerPosition.x * 600 * 0.25,
+      //     -currentPointerPosition.y * 600 * 0.25,
+      //     currentPointerPosition.z
+      //   )
+      // );
+      setPoints([
+        ...currentPoints,
+        new Vector3(
+          currentPointerPosition.x * 600 * 0.2,
+          -currentPointerPosition.y * 600 * 0.2,
+          currentPointerPosition.z
+        ),
+      ]);
+    }
+  }
+
+  window.addEventListener("pointermove", onPointerMove, false);
+
+  // window.addEventListener("mousedown", onPointerDown, false);
+
   useFrame(() => {
     controls.update();
+
+    const screenPosition = new Vector3(pointer.x, pointer.y, 0);
+    raycaster.setFromCamera(screenPosition, camera);
+    const instersections = raycaster.intersectObjects(scene.children);
+    if (instersections.length > 0) {
+      setCurrentPointerPosition(instersections[0].point);
+    }
+
     if (isLoaded) {
       for (const point of points) {
         const screenPosition = point.position.clone();
@@ -42,13 +102,12 @@ export const Geometry = ({ points }: GeometryProps): ReactElement => {
         pointDiv.style.opacity = "100%";
 
         raycaster.setFromCamera(screenPosition, camera);
-        const intersects = raycaster.intersectObjects(scene.children, true);
-        console.log(intersects, scene.children);
-        if (intersects.length === 0) {
+        const intersections = raycaster.intersectObjects(scene.children, true);
+        if (intersections.length === 0) {
           continue;
         }
 
-        const intersectionDistance = intersects[0].distance;
+        const intersectionDistance = intersections[0].distance;
         const pointDistance = point.position.distanceTo(camera.position);
 
         if (intersectionDistance < pointDistance) {
